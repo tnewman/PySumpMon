91af5659bd331375fed326577018bcbdef00a166
Thomas Newman <tnewman1@ltu.edu>
2015-10-19 00:27:12 -0400

    Invoke db_init() when db_init.py is run as a script.

--- dbinit.py
+++ dbinit.py
@@ -6,3 +6,6 @@ from sqlalchemy import create_engine
 def db_init():
     engine = create_engine(config.DB_CONNECTION_STRING)
     Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    db_init()
